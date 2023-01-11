f752546696363f2cffb7f3cbc530ae0c3c54cd67
QWERTYbae <nitwitghfj17717@protonmail.com>
2023-01-10 21:30:04 -0800

    Started game function

--- src/main.c
+++ src/main.c
@@ -12,17 +12,42 @@
 
 /* Posix */
 #include <unistd.h>
+#include <getopt.h>
 
 /* Curses */
 #include <ncurses.h>
 
 /* Macros */
+#define GAME_WINDOW_X ( COLS - 10 )
+#define GAME_WINDOW_Y ( LINES - 10 )
 
 /* Enums */
 enum {
         CursVis, 
         CursInvis
 };
+
+enum {
+        ColorBlack,
+        ColorBlue,
+        ColorGrey
+};
+
+/* Functions */
+static int32_t
+initColor ( void )
+{
+        if ( has_colors () )
+                start_color ();
+        else
+                return -1;
+
+        init_pair ( ColorBlack, COLOR_BLACK, COLOR_BLACK );
+        init_pair ( ColorBlue, COLOR_BLUE, COLOR_BLUE );
+        init_pair ( ColorGrey, COLOR_BLACK, COLOR_WHITE );
+
+        return 0;
+}
 
 static void
 exitNc ( void )
@@ -33,6 +58,14 @@ exitNc ( void )
 }
 
 static void
+errMsg ( const char* msg, int32_t code )
+{
+        exitNc ();
+        perror ( msg );
+        exit ( code );
+}
+
+static void
 initNc ( void )
 {
         initscr ();
@@ -40,6 +73,32 @@ initNc ( void )
         noecho ();
         keypad ( stdscr, TRUE );
         curs_set ( CursInvis );
+
+        if ( initColor () != 0 )
+                errMsg ( "Error: Colors are not supported by terminal\nExitting...\n", EXIT_FAILURE );
+}
+
+static void
+readInput ( int argc, char** argv )
+{
+        
+}
+
+static void
+game ( void )
+{
+        WINDOW* fWin, *bWin;
+
+        fWin = newwin ( GAME_WINDOW_Y, GAME_WINDOW_X, ( LINES - GAME_WINDOW_Y ) / 2, ( COLS - GAME_WINDOW_X ) / 2 );
+        bWin = newwin ( GAME_WINDOW_Y, GAME_WINDOW_X, ( LINES - GAME_WINDOW_Y ) / 2 + 1, ( COLS - GAME_WINDOW_X ) / 2 + 1 );
+
+        box ( bWin, 0, 0 );
+        box ( fWin, 0, 0 );
+
+        wrefresh ( bWin );
+        wrefresh ( fWin );
+
+        getch ();
 }
 
 int
@@ -48,7 +107,8 @@ main ( int argc, char** argv )
         setlocale ( LC_ALL, "" );
         initNc ();
 
-
+        readInput ( argc, argv );
+        game ();
 
         exitNc ();
 
